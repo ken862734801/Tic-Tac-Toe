1b7b65072d2bf6d9cdce28a79a6b82309125b6ee
Ken <103093259+ken862734801@users.noreply.github.com>
2022-09-14 01:45:16 -0400

    Add files via upload

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,20 @@
+let computerBtn = document.getElementById("computerBtn");
+let playerBtn = document.getElementById("playerBtn");
+let container = document.getElementById("container");
+let home = document.getElementById("home");
+
+let resetBtn = document.getElementById("resetBtn");
+
+function hideHome(){
+    container.style.display = "block";
+    home.style.display = "none";
+}
+function displayHome(){
+    container.style.display = "none";
+    home.style.display = "block";
+}
+
+playerBtn.addEventListener("click",hideHome);
+computerBtn.addEventListener("click", hideHome);
+
+resetBtn.addEventListener("click", displayHome);
