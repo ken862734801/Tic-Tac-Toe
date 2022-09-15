b9929a24a12dc22802934d68a76e4145d4aa31d6
Ken <103093259+ken862734801@users.noreply.github.com>
2022-09-15 02:48:59 -0400

    hide and display homepage functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Variables.
 let computerBtn = document.getElementById("computerBtn");
 let playerBtn = document.getElementById("playerBtn");
 let container = document.getElementById("container");
@@ -5,6 +6,7 @@ let home = document.getElementById("home");
 
 let resetBtn = document.getElementById("resetBtn");
 
+//Functions to hide and display the homepage.
 function hideHome(){
     container.style.display = "block";
     home.style.display = "none";
@@ -13,7 +15,6 @@ function displayHome(){
     container.style.display = "none";
     home.style.display = "block";
 }
-
 playerBtn.addEventListener("click",hideHome);
 computerBtn.addEventListener("click", hideHome);
 
