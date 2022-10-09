02037878b12c138a47bbcd3431303b398cfe9b67
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-10-09 17:34:01 -0400

    IT FINALLY WORKS LOL, fixed clear array function and gameover/winner booleans

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 const player = (name, marker) => {
     return {name, marker}
 }
+const playerOne = player("Player", "X");
+const playerTwo = player("Player", "O");
+let currentPlayer = playerOne;
+
 
 const gameBoard = (() => {
+    
+    let gameOver = false;
+    let winner = false;
 
-    let board = Array(9).fill("");
+    let board = new Array(9).fill("");
     console.log(board);
 
     const getBoard = () => {
         return board
     }
 
-    const clearBoard = () => {
-        board = Array(9).fill("");
+    const reset = () => board.fill("");
+
+    const resetWinner = () => {
+        winner = false;
+        return winner
     }
+
+    const resetGameOver = () => {
+        gameOver = false;
+        return gameOver
+    }
+
+   
 
     const markBoard = (marker, index) => {
-        if(board[index] === "X" || board[index] === "O"){
+        if(gameOver === true ){
             return
-        }else if(board[index] === ""){
-            board[index] = marker;
-        }
-        winConditionVertical();
-        winConditionDiagonal();
-        winConditionHorizontal();
-        drawCondition();
-        console.log(board);
-    }
+        }else if(board[index] === "X" || board[index] === "O"){
+            return
+        }else {
+            board[index] = marker; 
+            winConditionVertical();
+            winConditionHorizontal();
+            winConditionDiagonal();
+            drawCondition();
 
-    let gameOver = false;
-    let winner = false;
+            console.log(board);
+        }
+    };
 
     const getGameOver = () => {
-        let _gameOver = gameOver
-        return _gameOver
+        return gameOver
     }
     const getWinner = () => {
-        let _winner = winner 
         return winner
     }
 
@@ -44,20 +59,17 @@ const gameBoard = (() => {
         if(board[0] == gameController.getCurrentPlayer().marker && board[0] == board[3] && board[0]== board[6]){
             winner = true;
             gameOver = true;
-            console.log(gameOver);
-            console.log(winner);
+            return
             console.log("Player " + gameController.getCurrentPlayer().marker + " won!");
         }else if(board[1] == gameController.getCurrentPlayer().marker && board[1] == board[4] && board[1] == board[7]){
             winner = true;
             gameOver = true;
-            console.log(gameOver);
-            console.log(winner); 
+            return
             console.log("Player " + gameController.getCurrentPlayer().marker + " won!");
         }else if(board[2] == gameController.getCurrentPlayer().marker && board[2] == board[5] && board[2] == board[6]){
             winner = true;
             gameOver = true;
-            console.log(gameOver);
-            console.log(winner);
+            return
             console.log("Player " + gameController.getCurrentPlayer().marker + " won!");
         }
     }
@@ -113,29 +125,25 @@ const gameBoard = (() => {
     };
 
 
-    return {clearBoard, getBoard, getGameOver, getWinner, markBoard, drawCondition, winConditionDiagonal , winConditionHorizontal ,winConditionVertical}
+    return {  resetWinner,resetGameOver,reset, getBoard, getGameOver, getWinner, markBoard, drawCondition, winConditionDiagonal , winConditionHorizontal ,winConditionVertical}
 
 })();
 
 const gameController = (() => {
 
-    const playerOne = player("Player", "X");
-    const playerTwo = player("Player", "O");
-    let currentPlayer = playerOne;
 
 
     const getCurrentPlayer = () => {
-        let _currentPlayer = currentPlayer
-            return _currentPlayer
+            return currentPlayer
     }
 
     const playerTurn = () => {
-        if(currentPlayer == playerOne){
-            currentPlayer = playerTwo;
-            console.log(currentPlayer);
+        if(gameBoard.getGameOver() === true){
+            return;
+        }else if(currentPlayer == playerOne){
+            currentPlayer = playerTwo;  
         }else{
             currentPlayer = playerOne;
-            console.log(currentPlayer);
         }
     };
 
@@ -149,12 +157,12 @@ const gameController = (() => {
 
     const resetGame = () => {
         gameBoard.clearBoard();
-        currentPlayer = playerOne;
-        gameBoard.getGameOver = false;
-        gameBoard.getWinner = false;
+        gameBoard.getGameOver() === false;
+        gameBoard.getWinner() === false;
+     
     }
 
-    return {getCurrentPlayer,playerTurn, resetGame}
+    return {playerOne, playerTwo, getCurrentPlayer,playerTurn, resetGame}
 })();
 
 const displayController = (() => {
@@ -162,15 +170,18 @@ const displayController = (() => {
     let messageDisplay = document.getElementById("message-display");
 
     let squares = document.querySelectorAll(".square");
-    let refreshBtn = document.getElementById("refreshBtn");
+    let refreshBtn = document.getElementById("restartBtn");
 
     squares.forEach(square => {
         square.addEventListener("click", (e)=> {
             drawMarker(e);
             changeColor(e);
             gameBoard.markBoard(gameController.getCurrentPlayer().marker, getSquareIndex(e));
+            gameController.getCurrentPlayer();
             gameController.playerTurn();
-            determineMessage(e);
+            determineMessage();
+            console.log(gameBoard.getBoard())
+            console.log(gameController.getCurrentPlayer())
            
         });
     });
@@ -182,13 +193,15 @@ const displayController = (() => {
     };
 
     const drawMarker = (e) => {
-        if(e.target.textContent === "X" || e.target.textContent === "O"){
-            e.target.removeEventListener("click")
-            return
-        }else if(e.target.textContent === ""){
-            e.target.textContent = gameController.getCurrentPlayer().marker;
+        if(gameBoard.getGameOver() === true || e.target.textContent !== ""){
+            squares.forEach(square => {
+                square.removeEventListener("click", gameController.playerTurn())
+            })
+            return;
+        }else{
+             e.target.textContent = gameController.getCurrentPlayer().marker;
         }
-    }
+     }
 
     const changeColor = (e) => {
         if(e.target.textContent === "X"){
@@ -197,23 +210,33 @@ const displayController = (() => {
             e.target.style.color = "#02adc7";
         }
     }
-    const clearGameDisplay = () => {
-        for(i = 0; i < squares.length; i++){
-            squares[i].textContent = "";
-            gameController.resetGame();
-            messageDisplay.textContent = "It's X's turn."
-        }
-    };
+  
 
-    const determineMessage = (e) => {
+    const determineMessage = () => {
         if(gameBoard.getGameOver() === true && gameBoard.getWinner() === true){
-            messageDisplay.textContent = e.target.textContent + " has won.";
+            messageDisplay.textContent = gameController.getCurrentPlayer().marker + " has won.";
+            return;
         }else if (gameBoard.getGameOver() === true && gameBoard.getWinner() === false){
             messageDisplay.textContent = "It's a draw.";
         }else if(gameBoard.getGameOver() === false && gameBoard.getWinner() === false){
             messageDisplay.textContent = "It's " + gameController.getCurrentPlayer().marker + "'s turn.";
         }
+    
     }
+
+        
+    const clearGameDisplay = () => {
+        gameBoard.resetWinner();
+        gameBoard.resetGameOver();
+        gameBoard.reset();
+    for(i = 0; i < squares.length; i++){
+        squares[i].textContent = "";
+    };
+        messageDisplay.textContent = "It's X's turn.";
+        gameController.playerTurn();
+        currentPlayer = playerOne;
+        console.log(gameBoard.getBoard())
+};
 
     refreshBtn.addEventListener("click", clearGameDisplay);
 
